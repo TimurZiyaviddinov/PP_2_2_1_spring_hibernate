9235c785ed0e8a8542f505d102e1c96d80837316
timur_ziya <timka_ziya@mail.ru>
2022-11-30 18:14:38 +0300

    a

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -18,25 +18,25 @@ public class MainApp {
         UserService userService = context.getBean(UserService.class);
         CarService carService = context.getBean(CarService.class);
 
-//        Car car1 = new Car("Car1", 100);
-//        Car car2 = new Car("Car2", 200);
-//        Car car3 = new Car("Car3", 300);
-//        Car car4 = new Car("Car4", 400);
-//
-//        userService.add(new User("User1", "Lastname1", "user1@mail.ru", car1));
-//        userService.add(new User("User2", "Lastname2", "user2@mail.ru", car2));
-//        userService.add(new User("User3", "Lastname3", "user3@mail.ru", car3));
-//        userService.add(new User("User4", "Lastname4", "user4@mail.ru", car4));
-//
-//        List<User> users = userService.listUsers();
-//        for (User user : users) {
-//            System.out.println("Id = " + user.getId());
-//            System.out.println("First Name = " + user.getFirstName());
-//            System.out.println("Last Name = " + user.getLastName());
-//            System.out.println("Email = " + user.getEmail());
-//            System.out.println("Car model = " + user.getCar().getModel() + ",Car series = " + user.getCar().getSeries());
-//            System.out.println();
-//        }
+        Car car1 = new Car("Car1", 100);
+        Car car2 = new Car("Car2", 200);
+        Car car3 = new Car("Car3", 300);
+        Car car4 = new Car("Car4", 400);
+
+        userService.add(new User("User1", "Lastname1", "user1@mail.ru", car1));
+        userService.add(new User("User2", "Lastname2", "user2@mail.ru", car2));
+        userService.add(new User("User3", "Lastname3", "user3@mail.ru", car3));
+        userService.add(new User("User4", "Lastname4", "user4@mail.ru", car4));
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Car model = " + user.getCar().getModel() + ",Car series = " + user.getCar().getSeries());
+            System.out.println();
+        }
 
         Car car = new Car("Car3", 300);
         User user = carService.getUserByCar(car);
